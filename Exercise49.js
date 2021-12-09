3ba7bda8e5f2271ceeafb581aa7b2c0fa88538a8
Ruiyue Wang <phynew@hotmail.com>
2021-12-09 01:04:07 -0500

    Update Exercise49.js

--- Exercise49.js
+++ Exercise49.js
@@ -1,3 +1,11 @@
+// Solution 1
+function validUserNames(usernames) {
+    return usernames.filter(function(username) {
+        return username.length < 10;
+    })
+}
+
+// Solution 2
 function validUserNames(usernames) {
     return usernames.filter(username => {
         return username.length < 10;
