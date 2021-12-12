a6461f8beeefeabe6d48f7e79d92430c533e1192
Ruiyue Wang <phynew@hotmail.com>
2021-12-12 18:44:09 -0500

    Create Exercise54.js

---
+++ Exercise54.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+const link = document.querySelector("img");
+// link.src = "https://devsprouthosting.com/images/chicken.jpg";
+link.setAttribute("src", "https://devsprouthosting.com/images/chicken.jpg");
+link.setAttribute("alt", "chicken");
