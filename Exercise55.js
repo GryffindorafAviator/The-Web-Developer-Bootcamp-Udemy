e3a60deefa0f157be12a614c5aa8c5b69dcb84bc
Ruiyue Wang <phynew@hotmail.com>
2021-12-12 19:20:37 -0500

    Create Exercise55.js

---
+++ Exercise55.js
@@ -0,0 +1,6 @@
+const text = document.querySelector("div[id='container']");
+const image = document.querySelector("img");
+
+text.style.textAlign="center";
+image.style.width="150px";
+image.style.borderRadius="50%";
