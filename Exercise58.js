297dd32b0d3db763044202f0876d5ce85b979fd7
Ruiyue Wang <phynew@hotmail.com>
2021-12-25 20:51:27 -0500

    Create Exercise58.js

---
+++ Exercise58.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 100; i++) {
+    const div = document.querySelector("div")
+    const bn = document.createElement("Button")
+    
+    bn.innerText = "Dora"
+    div.appendChild(bn)
+}
