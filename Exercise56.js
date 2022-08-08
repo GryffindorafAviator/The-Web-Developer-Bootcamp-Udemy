e70ca5d490da7906a314035888d983e6fbbc04f2
Ruiyue Wang <phynew@hotmail.com>
2022-08-08 16:26:08 -0400

    Update Exercise56.js

--- Exercise56.js
+++ Exercise56.js
@@ -3,6 +3,6 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 //YOU CODE GOES HERE:
 const letters = document.querySelectorAll("span");
 
-for (let i = 0; i < 7;i++) {
+for (let i = 0; i < 7; i++) {
     letters[i].style.color=colors[i];
 }
